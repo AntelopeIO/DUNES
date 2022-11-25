099e9b758252d6bb03c2b2a6d54342106dd1ce30
Michal Lesiak <michal.lesiak@eosnetwork.com>
2022-11-25 16:50:58 +0100

    Add possibility to change leap/CDT version.
    Example command to build a DUNE image with specific versions:
    docker build --no-cache --build-arg USER_ID=0 --build-arg GROUP_ID=0 --build-arg CDT_VERSION=3.1.0-rc1 --build-arg LEAP_VERSION=3.2.0 -f Dockerfile.unix -t dune
    
    Alternatively run bootstrap.sh script with arguments, i.e.
    ./bootstrap.sh --leap=3.2.0 --cdt=3.1.0-rc1

--- scripts/bootstrap_env.sh
+++ scripts/bootstrap_env.sh
@@ -3,25 +3,32 @@
 ARCH=`uname -m`
 
 ORG="AntelopeIO"
-NODE_VERSION="3.1.0"
-CDT_VERSION="3.0.0"
 
+FINAL_LEAP_VERSION="3.1.0"
+FINAL_CDT_VERSION="3.0.0"
+
+if [ -n "$LEAP_VERSION" ]; then
+   FINAL_LEAP_VERSION="$LEAP_VERSION"
+fi
+if [ -n "$CDT_VERSION" ]; then
+   FINAL_CDT_VERSION="$CDT_VERSION"
+fi
 
 CONTAINER_PACKAGE=AntelopeIO/experimental-binaries
 GH_ANON_BEARER=$(curl -s "https://ghcr.io/token?service=registry.docker.io&scope=repository:${CONTAINER_PACKAGE}:pull" | jq -r .token)
-curl -s -L -H "Authorization: Bearer ${GH_ANON_BEARER}" https://ghcr.io/v2/${CONTAINER_PACKAGE}/blobs/$(curl -s -L -H "Authorization: Bearer ${GH_ANON_BEARER}" https://ghcr.io/v2/${CONTAINER_PACKAGE}/manifests/v${NODE_VERSION} | jq -r .layers[0].digest) | tar -xz
+curl -s -L -H "Authorization: Bearer ${GH_ANON_BEARER}" https://ghcr.io/v2/${CONTAINER_PACKAGE}/blobs/$(curl -s -L -H "Authorization: Bearer ${GH_ANON_BEARER}" https://ghcr.io/v2/${CONTAINER_PACKAGE}/manifests/v${FINAL_LEAP_VERSION} | jq -r .layers[0].digest) | tar -xz
 
 if [ "${ARCH}" = "x86_64" ]; then
-   wget https://github.com/${ORG}/leap/releases/download/v${NODE_VERSION}/leap-${NODE_VERSION}-ubuntu20.04-x86_64.deb
-   apt install ./leap-${NODE_VERSION}-ubuntu20.04-x86_64.deb
-   apt install ./leap-dev-${NODE_VERSION}-ubuntu20.04-x86_64.deb
-   wget https://github.com/${ORG}/cdt/releases/download/v${CDT_VERSION}/cdt_${CDT_VERSION}_amd64.deb
-   apt install ./cdt_${CDT_VERSION}_amd64.deb
+   wget https://github.com/${ORG}/leap/releases/download/v${FINAL_LEAP_VERSION}/leap-${FINAL_LEAP_VERSION}-ubuntu20.04-x86_64.deb
+   apt install ./leap-${FINAL_LEAP_VERSION}-ubuntu20.04-x86_64.deb
+   apt install ./leap-dev-${FINAL_LEAP_VERSION}-ubuntu20.04-x86_64.deb
+   wget https://github.com/${ORG}/cdt/releases/download/v${FINAL_CDT_VERSION}/cdt_${FINAL_CDT_VERSION}_amd64.deb
+   apt install ./cdt_${FINAL_CDT_VERSION}_amd64.deb
 else
-   apt install ./leap-${NODE_VERSION}-ubuntu20.04-aarch64.deb
-   apt install ./leap-dev-${NODE_VERSION}-ubuntu20.04-aarch64.deb
-   wget https://github.com/${ORG}/cdt/releases/download/v${CDT_VERSION}/cdt_${CDT_VERSION}_arm64.deb
-   apt install ./cdt_${CDT_VERSION}_arm64.deb
+   apt install ./leap-${FINAL_LEAP_VERSION}-ubuntu20.04-aarch64.deb
+   apt install ./leap-dev-${FINAL_LEAP_VERSION}-ubuntu20.04-aarch64.deb
+   wget https://github.com/${ORG}/cdt/releases/download/v${FINAL_CDT_VERSION}/cdt_${FINAL_CDT_VERSION}_arm64.deb
+   apt install ./cdt_${FINAL_CDT_VERSION}_arm64.deb
 fi
 
 git clone https://github.com/${ORG}/reference-contracts
