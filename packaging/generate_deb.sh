f450437d068a440e4d8c934d4a76d7c9c1a1fef1
Michal Lesiak <michal.lesiak@eosnetwork.com>
2022-12-05 10:54:54 +0100

    Add packaging of DUNE

---
+++ packaging/generate_deb.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+. ./generate_common.sh
+
+OS=$1
+ARCH=$2
+
+PREFIX="usr"
+SPREFIX="$PREFIX"
+SUBPREFIX="opt/$PROJECT/$VERSION"
+SSUBPREFIX="opt\/$PROJECT\/$VERSION"
+RELEASE="$VERSION_SUFFIX"
+
+# default release to "1" if there is no suffix
+if [[ -z $RELEASE ]]; then
+  RELEASE="1"
+fi
+
+NAME="${PROJECT}_${VERSION_NO_SUFFIX}-${RELEASE}_${ARCH}"
+
+mkdir -p "$PROJECT"/DEBIAN
+echo "Package: $PROJECT
+Version: $VERSION_NO_SUFFIX-$RELEASE
+Section: devel
+Priority: optional
+Architecture: $ARCH
+Homepage: $URL
+Maintainer: $EMAIL
+Description: $DESC" &> "$PROJECT"/DEBIAN/control
+cat "$PROJECT"/DEBIAN/control
+
+export PREFIX
+export SUBPREFIX
+export SPREFIX
+export SSUBPREFIX
+
+. ./generate_tarball.sh "$NAME" "$OS"
+echo "Unpacking tarball: $NAME.tar.gz..."
+tar -xzvf "$NAME".tar.gz -C "$PROJECT" || exit 1
+dpkg-deb --build "$PROJECT" || exit 1
+mv "$PROJECT".deb "$NAME".deb || exit 1
+rm -r "$PROJECT" || exit 1
+
+exit 0
