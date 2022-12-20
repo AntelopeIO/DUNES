028bd714ab90a9034f51bb8bf81e945f1c6fdcb4
Michal Lesiak <michal.lesiak@eosnetwork.com>
2022-12-20 15:05:32 +0100

    Adding docker-ce-cli which contains docker command.

--- packaging/generate_deb.sh
+++ packaging/generate_deb.sh
@@ -19,7 +19,7 @@ NAME="${PACKAGE_NAME}_${VERSION_NO_SUFFIX}-${RELEASE}_all"
 mkdir -p "$PROJECT"/DEBIAN
 echo "Package: $PACKAGE_NAME
 Version: $VERSION_NO_SUFFIX-$RELEASE
-Depends: python3, docker
+Depends: python3, docker | docker-ce-cli
 Section: devel
 Priority: optional
 Architecture: all
