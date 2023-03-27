1f5434e6a913db579355dbd7079066afdc18bbdc
Michal Lesiak <michal.lesiak@eosnetwork.com>
2023-03-27 09:16:41 +0200

    Fix tests

--- tests/common.py
+++ tests/common.py
@@ -10,6 +10,6 @@ DUNE_EXE = os.path.split(TEST_PATH)[0] + "/dune"
 print("Executable path: ", DUNE_EXE)
 
 # Default addresses
-DEFAULT_HTTP_ADDR="127.0.0.1:8888"
+DEFAULT_HTTP_ADDR="0.0.0.0:8888"
 DEFAULT_P2P_ADDR="0.0.0.0:9876"
-DEFAULT_SHIP_ADDR="127.0.0.1:8080"
+DEFAULT_SHIP_ADDR="0.0.0.0:8080"
