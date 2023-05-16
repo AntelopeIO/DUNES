ffb5e467cfe939d414c7a6fe264989aabff75285
Scott Bailey <Scott@TheBaileyFamily.org>
2023-05-16 14:29:38 -0500

    boostrap.sh now tags the release with it's git commit short hash.

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 set -o errexit -o pipefail -o noclobber -o nounset
 
+set -v
+
 getopt --test && echo "'getopt --test' failed in this environment." && exit 1
 
 # Set SDIR to the path containing this script. Even if it's a symlink.
@@ -38,6 +40,7 @@ while true; do
             ;;
         -r|--release)
             RELEASE_VERSION=$(${SDIR}/dune --version-short)
+            echo "Release version: ${RELEASE_VERSION}"
             shift 1
             ;;
         --)
@@ -61,18 +64,17 @@ fi
 docker build --no-cache --build-arg USER_ID=0 --build-arg GROUP_ID="$GROUP_ID" $LEAP_ARGUMENT $CDT_ARGUMENT -f Dockerfile.unix -t dune "$SDIR"
 
 if [ -n "${RELEASE_VERSION}" ]; then
-    docker tag dune:${RELEASE_VERSION}
+    docker tag dune dune:${RELEASE_VERSION}
     docker tag dune ghcr.io/antelopeio/dune:latest
     docker tag dune ghcr.io/antelopeio/dune:${RELEASE_VERSION}
     echo "Tagged image with latest and version '${RELEASE_VERSION}'."
-else
-    GIT_CMD="git -C ${SDIR} rev-parse --short HEAD"
-    if ${GIT_CMD} > /dev/null 2> /dev/null; then
-	COMMIT_HASH=$(${GIT_CMD})
-        docker tag dune dune:${COMMIT_HASH}
-        echo "Tagged image with commit hash '${COMMIT_HASH}'."
-    else
-        echo "Could not determine git commit hash for this image. Not tagging."
-    fi
+fi
 
+GIT_CMD="git -C ${SDIR} rev-parse --short HEAD"
+if ${GIT_CMD} > /dev/null 2> /dev/null; then
+    COMMIT_HASH=$(${GIT_CMD})
+    docker tag dune dune:${COMMIT_HASH}
+    echo "Tagged image with commit hash '${COMMIT_HASH}'."
+else
+    echo "Could not determine git commit hash for this image. Not tagging."
 fi
