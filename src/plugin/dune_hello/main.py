b14d8307bf1aaca32976fd11ce146e454ae20887
Bucky Kittinger <bucky.kittinger@eosnetwork.com>
2023-05-17 14:40:07 -0400

    oops

--- src/plugin/dune_hello/main.py
+++
@@ -1,7 +0,0 @@
-def handle_args(args):
-    if args.hello:
-        print('Hello from DUNE plugin!')
-
-def add_parsing(parser):
-    parser.add_argument('--hello', action='store_true',
-                                  help='outputs "Hello World"')
