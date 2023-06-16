1534bcf1e36da69726bb85ef0ed18d2532d66003
Scott Bailey <Scott@TheBaileyFamily.org>
2023-06-16 15:29:12 -0500

    Make the dirtydb fix backwards compatible.

--- scripts/start_node.sh
+++ scripts/start_node.sh
@@ -3,7 +3,7 @@
 # 1 - data directory
 # 2 - config.ini directory
 # 3 - snapshot
-# 4 - replay-blockchain option
-# 5 - node name
+# 4 - node name
+# 5 - replay-blockchain option
 
-nodeos --data-dir=$1 --config-dir=$2 $3 $4 >/app/$5.out 2>&1 &
+nodeos --data-dir=$1 --config-dir=$2 $3 $5 >/app/$4.out 2>&1 &
